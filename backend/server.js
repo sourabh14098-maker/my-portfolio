93c8ffacc58f39334db37b69a5c7644c0536728c
Sourabh Raj <sourabh14098@gmail.com>
2026-06-06 10:17:56 +0530

    add backend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
+
+const contactRoutes = require("./routes/contactRoutes");
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/contact", contactRoutes);
+
+// MongoDB Connection
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB Connected ✅"))
     .catch((err) => console.log(err));
 
+// Test Route
 app.get("/", (req, res) => {
     res.send("NYXO Backend Running 🚀");
 });
 
+// Server Start
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
